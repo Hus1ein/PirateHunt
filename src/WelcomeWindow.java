e8ac6a8a71c488a1103b059f26d0a974d9fc057c
HussainAbdelilah <hussain.abdelilah@hotmail.com>
2019-01-18 14:00:00 +0100

    fixing some bugs

--- src/WelcomeWindow.java
+++ src/WelcomeWindow.java
@@ -22,7 +22,7 @@ public class WelcomeWindow {
         mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(7, 1));
+        mainPanel.setLayout(new GridLayout(8, 1));
 
 
         JLabel welcomeMessage = new JLabel("Welcome in Pirate Hunt", SwingConstants.CENTER);
@@ -48,12 +48,39 @@ public class WelcomeWindow {
         JButton startNewGame = new JButton("Start new game");
         startNewGame.setBackground(new Color(77, 148, 255));
 
+        JButton getHelpButton = new JButton("Help");
+
         startNewGame.addActionListener(e -> {
             if (!playerNameInput.getText().trim().equals("")) {
                 if (helperInterface != null) {
                     helperInterface.openPlayWindow(playerNameInput.getText().trim(), levelsList.getSelectedIndex() + 1);
                 }
             }
+        });
+
+        getHelpButton.addActionListener(e -> {
+            JFrame help = new JFrame("Help");
+            help.setSize(850, 750);
+            help.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+            JPanel helpPanel = new JPanel();
+            JLabel gameHelp = new JLabel();
+            gameHelp.setText("" +
+                    "<html>" +
+                    "<h1>Upustvo</h1>" +
+                    "<h2>1. Igra se igra na matrici 15x15 sa preprekama.</h2>" +
+                    "<h2>2. Cilj igrača je doći iz jednog polja u drugo. </h2>" +
+                    "<h2>3. Gusari pokušavaju uhvatiti igrača.</h2>" +
+                    "<h2>4. Ako gusar udari u prepreku, nestaje.</h2>" +
+                    "<h2>5. Ako se dva gusara sudare, nastane nova prepreka.</h2>" +
+                    "<h2>6. Brod se kreće pomoću keyboard</h2>" +
+                    "</html>"
+            );
+
+
+            helpPanel.add(gameHelp);
+            help.add(helpPanel);
+            help.setResizable(false);
+            help.setVisible(true);
         });
 
 
@@ -63,6 +90,7 @@ public class WelcomeWindow {
         mainPanel.add(level);
         mainPanel.add(levelsList);
         mainPanel.add(startNewGame);
+        mainPanel.add(getHelpButton);
 
 
         mainFrame.add(mainPanel);
